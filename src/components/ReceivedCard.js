fffd438f05697df1ba063250e830ddea2b19ba67
Jason Smith <jasons@alphaworks.tech>
2021-12-16 10:10:37 -0800

    feat(received card): build layout for received card.

---
+++ src/components/ReceivedCard.js
@@ -0,0 +1,31 @@
+function ReceivedCard({Greeting, Body, Closing, GreetingImage, setDisplay}) {
+
+    const goToLanding = () => {
+        setDisplay("landing");
+    }
+
+    return (
+        <div id="RecievedCard">
+            <div className="img-wrap">
+				<img src={GreetingImage} alt="" />
+			</div>
+            <div className="message-wrap">
+                <p className="greeting-wrap">
+                    {Greeting} <span className="comma">,</span>
+                </p>
+                <p className="body-wrap">
+                    {Body}
+                </p>
+                <p className="closing-wrap">
+                    {Closing}
+                </p>
+            </div>
+            
+            <div className="btn-wrap">
+                <button onClick={goToLanding}>Like this card? Send one to someone.</button>
+            </div>
+        </div>
+    );
+}
+
+export default ReceivedCard;
